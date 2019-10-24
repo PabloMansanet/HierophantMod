46f59b271229bdf8e36deb16a4d8d69e5e9e7ae1
PabloMansanet <pablomansanet@gmail.com>
2019-10-24 08:40:44 +0200

    Nerf Generosity

--- mod/src/main/java/hierophant/cards/Generosity.java
+++ mod/src/main/java/hierophant/cards/Generosity.java
@@ -1,32 +1,31 @@
 package hierophant.cards;
 
+import static hierophant.HierophantMod.makeCardPath;
+
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
 import hierophant.HierophantMod;
-import hierophant.tags.HierophantTags;
 import hierophant.characters.Hierophant;
 import hierophant.powers.GenerosityPower;
-
-import static hierophant.HierophantMod.makeCardPath;
+import hierophant.tags.HierophantTags;
 
 public class Generosity extends AbstractDynamicCard {
 
     public static final String ID = HierophantMod.makeID(Generosity.class.getSimpleName());
     public static final String IMG = makeCardPath("Generosity.png");
 
-    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = Hierophant.Enums.COLOR_GOLD;
 
     private static final int COST = 1;
+    private static final int UPGRADED_COST = 0;
 
     public Generosity() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
@@ -43,8 +42,7 @@ public class Generosity extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            this.isInnate = true;
-            this.rawDescription = UPGRADE_DESCRIPTION;
+            upgradeBaseCost(UPGRADED_COST);
             initializeDescription();
         }
     }
